23e5a5068e00e092d8c97b9b841ec1f6dfe2bc1b
Ev357 <evest.dev@gmail.com>
2025-07-02 02:23:17 +0200

    ci: cachix

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   nixConfig = {
     extra-substituters = [
+      "https://tuisic.cachix.org"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "tuisic.cachix.org-1:XEafqLtuNhcRjCZERfPbKp+xSAqDC9Gfd0O7gzFpaSY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
