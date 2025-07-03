e4f0dd3c0ea76a1f1c142fb640eb39c1f9256ed2
Ev357 <evest.dev@gmail.com>
2025-07-04 00:31:35 +0200

    feat: added nix types for providers

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -6,6 +6,37 @@
     let
       cfg = config.programs.tuisic;
       tuisic = pkgs.callPackage ../pkgs/tuisic.nix { inherit inputs; };
+
+      localProviderType = lib.types.submodule {
+        options = {
+          type = lib.mkOption {
+            type = lib.types.enum [ "local" ];
+            description = "Local provider type";
+          };
+
+          path = lib.mkOption {
+            type = lib.types.str;
+            description = "Path to local music directory";
+          };
+        };
+      };
+
+      providerType = lib.types.oneOf [ localProviderType ];
+
+      settingsType = lib.types.submodule {
+        options = {
+          providers = lib.mkOption {
+            type = lib.types.listOf providerType;
+            default = [
+              {
+                type = "local";
+                path = "~/Music";
+              }
+            ];
+            description = "List of music providers";
+          };
+        };
+      };
     in
     {
       options.programs.tuisic = {
@@ -24,7 +55,7 @@
         };
 
         settings = lib.mkOption {
-          type = lib.types.attrs;
+          type = settingsType;
           default = { };
           description = "Settings";
         };
